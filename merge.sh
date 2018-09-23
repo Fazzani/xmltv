04766b8c9b21fc3ca6586abcaaccfb116a6ef71c
Fazzani <tunisenheni@gmail.com>
2018-09-23 20:00:35 +0200

    daily

--- merge.sh
+++ merge.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+source $(dirname $0)/.conf
+
 set -ex
 
-#command -v tv_merge >/dev/null 2>&1 || { echo >&2 "I require tv_merge but it's not installed. Please install xmltv-util package. Aborting."; exit 1; }
+command -v tv_merge >/dev/null 2>&1 || { echo >&2 "I require tv_merge but it's not installed. Please install xmltv-util package. Aborting."; exit 1; }
 command -v tar >/dev/null 2>&1 || { echo >&2 "I require tar but it's not installed.  Aborting."; exit 1; }
 
 output="output.xmltv"
@@ -21,11 +23,14 @@ do
    fi
 
    echo -e "merge $second ${filename}"
-   /usr/bin/tv_merge -i  $second -m $filename -o $output
+   /usr/bin/tv_merge -i $second -m $filename -o $output
    i=$((i+1))
 
 done;
 
-tar zcvf output.tar.gz $output
+#tar zcvf output.tar.gz $output
+mv $output merge.xmltv
+git remote add origin2 https://${GITHUB_API_TOKEN}@github.com/fazzani/xmltv.git > /dev/null 2>&1
+git add merge.xmltv && git commit -m "daily pushing merged xmltv file to GITHUB" && git push origin2 HEAD:master
 
 exit 0
