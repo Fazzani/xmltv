5eae87b9d1b16e03e959869b746f4d616d220cda
Fazzani <tunisenheni@gmail.com>
2018-09-23 17:42:42 +0200

    daily

--- merge.sh
+++ merge.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 command -v tv_merge >/dev/null 2>&1 || { echo >&2 "I require tv_merge but it's not installed. Please install xmltv-util package. Aborting."; exit 1; }
 command -v tar >/dev/null 2>&1 || { echo >&2 "I require tar but it's not installed.  Aborting."; exit 1; }
 
